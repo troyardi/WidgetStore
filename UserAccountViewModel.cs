e90ca5e83e24c1250b0c9a2dc5698254e5305d6e
troyardi <Troydiedwardo@gmail.com>
2017-03-06 17:19:42 -0500

    Add files via upload

---
+++ UserAccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WidgetStore.ViewModels
+{
+    public class UserAccountViewModel
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
